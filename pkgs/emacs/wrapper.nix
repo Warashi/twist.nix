2c56e958756c96df98fa10109f14173e7e5c3a64
Shinnosuke Sawada <6warashi9@gmail.com>
2024-03-23 06:51:27 +0900

    wip

--- pkgs/emacs/wrapper.nix
+++ pkgs/emacs/wrapper.nix
@@ -187,7 +187,7 @@ in
         --set EMACSLOADPATH "$siteLisp:"
       fi
     done
-  '' + lib.optionalString withNS ''
+
     mkdir -p $out/Applications
     mv nextstep/Emacs.app $out/Applications
   ''
